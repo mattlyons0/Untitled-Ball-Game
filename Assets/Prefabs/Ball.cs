f374964b7c47822f351761083e517368e46ace16
Daniel Worsnup <dworsnup@squarespace.com>
2019-09-28 00:00:44 -0400

    Add Ball prefab, add GameManager to GameScene

---
+++ Assets/Prefabs/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Vector2 direction;
+
+    void Start()
+    {
+        direction = Vector2.right.normalized;
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
