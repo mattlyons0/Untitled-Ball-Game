f343510e3117df6b5f92c62659fd693cbad1f8fe
Trent Rand <trand@squarespace.com>
2019-09-30 00:25:03 -0400

    Add basis for player paddle drawing
    To begin the process of developing an in-game player input controller, this
    prototype uses the LineRenderer component to draw lines spanning from the users
    mouse-down coordinate to the coordinate of the mouse-up event.
    
    The next step will involve making these rendered lines act as colliders; and if
    successful, the PlayerInput controller will be extended to add features such as
    max line length and a limit on the number of drawable lines.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerInput : MonoBehaviour
+{
+  public LineRenderer lineRenderer;
+  public Material lineMaterial;
+  private int numOfLines = 0;
+
+  private Vector3 initialMouseDownPosition;
+  private bool prevMouseDown = false;
+
+  void Update()
+  {
+    bool isMouseDown = Input.GetMouseButton(0);
+    if (isMouseDown && !prevMouseDown) {
+      handleMouseDown();
+    } else if (!isMouseDown && prevMouseDown) {
+      handleMouseUp();
+    } else if (isMouseDown && prevMouseDown) {
+      handleDrag();
+    }
+    prevMouseDown = isMouseDown;
+  }
+
+  public void handleMouseDown()
+  {
+    if (lineRenderer == null) {
+      initializeLineRenderer();
+    }
+
+    initialMouseDownPosition = getActualMousePosition();
+    lineRenderer.SetPosition(0, initialMouseDownPosition);
+    lineRenderer.SetPosition(1, initialMouseDownPosition);
+  }
+
+  public void handleDrag()
+  {
+    initialMouseDownPosition = getActualMousePosition();
+    lineRenderer.SetPosition(1, initialMouseDownPosition);
+  }
+
+  public void handleMouseUp()
+  {
+    initialMouseDownPosition = getActualMousePosition();
+    lineRenderer.SetPosition(1, initialMouseDownPosition);
+    lineRenderer = null;
+    numOfLines++;
+  }
+
+  private void initializeLineRenderer()
+  {
+    lineRenderer = new GameObject("PlayerWall-" + numOfLines).AddComponent<LineRenderer>();
+    lineRenderer.material = lineMaterial;
+    lineRenderer.positionCount = 2;
+    lineRenderer.startWidth = 0.1f;
+    lineRenderer.endWidth = 0.1f;
+    lineRenderer.useWorldSpace = false;
+  }
+
+  private Vector3 getActualMousePosition()
+  {
+    Vector3 inputMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    inputMousePosition.z = 0;
+
+    return inputMousePosition;
+  }
+}
